54fda3fc3e4bc81f613470bd6244e75832e92eb0
Josh Channings <randomblast@googlemail.com>
2010-03-05 18:47:50 +0000

    Added parser loop and PCP_Property class

--- pcp.php
+++ pcp.php
@@ -15,6 +15,7 @@
 		private $state = array(
 			  'sources' => array()
 			, 'variables' => array()
+			// $state['selectors']['div.foo#bar'] = array(PCP_Property, PCP_Property, ...)
 			, 'selectors' => array()
 		);
 
@@ -45,7 +46,131 @@
 		{
 			// Clear the state
 			$state['variables'] = array();
-			$state['selectors'] = array();
+			$state['selectors'] = array(); // $state['selectors']['div.foo#bar>p a:hover']['width']
+
+			foreach($state['sources'] as $src)
+			{
+				if(false !== ($fd = fopen($src, 'r')))
+				{
+					$ln = 0; $cn = 0;			// Line number, Column number
+					$buf = '';				// Chomped input
+					$selector = array();	// Stack of working selectors
+					$p;						// Current property. @see PCP_Property
+					$fstate = array();		// Per-file state. @see $state
+
+					while(false !== ($c = fgetc($fd)))
+					{
+						switch($c)
+						{
+							case '{':	// selector { properties
+
+								// Convert buffer into selector, then clear
+								if(false === ($sel = clean_selector($buf))) fclose($fd);
+								$buf = '';
+
+								// Add selector to stack
+								if(count($selector))
+									array_push($selector, end($selector).'>'.$sel);
+								else
+									array_push($selector, $sel);
+
+								break;
+
+							case '}':	// properties }
+								
+								// Check for no open selectors
+								if(count($selector) < 1)
+								{
+									trigger_error("$src:$ln:$cn: Unexpected '}'", E_USER_ERROR);
+									fclose($fd);
+								} else
+								{
+									// Check for open property
+									if($p)
+									{	
+										// Check for potential value in the buffer
+										if(strlen(trim($buf)))
+										{
+											trigger_error(
+												  "$src:$ln:$sc: Property not closed properly. "
+												 ."Assumed '{$p->name}: $buf'"
+												, E_USER_WARNING
+											);
+
+										} else
+										{
+											trigger_error(
+												  "$src:$ln:$cn: Selector closed with open property"
+												, E_USER_ERROR
+											);
+											fclose($fd);
+										}
+									}
+									array_pop($selector);
+								}
+
+								break;
+
+							case ':':	// property name : property value
+
+								// Is there already an open property?
+								if($p)
+									trigger_error(
+										  "$src:$ln:$sc: Found ':' in property value"
+										, E_USER_WARNING
+									);
+								else
+								{
+									$p->src = $src;
+									$p->ln = $ln;
+									$p->cn = $cn;
+
+									$fstate[end($selector)][$p->name] = &$p;
+								}
+
+								break;
+
+							case ';':	// property value ;
+
+								if($p)
+								{
+									$p->value = trim($buf);
+									$buf = '';
+									unset($p);
+								} else
+								{
+									trigger_error(
+										  "$src:$ln:$cn: Found ';' with no open property"
+										, E_USER_ERROR
+									);
+									fclose($fd);
+								}
+
+								break;
+
+							case "\n":	// Newline
+								$buf .= ' ';
+								$ln++;
+								$cn = 0;
+								break;
+
+							default:	// chomp
+								$buf .= $c;
+								$cn++;
+								break;
+						}
+
+					} // while(fgetc())
+
+					// Check file is still open (didn't bomb)
+					if($fd)
+					{
+						// Merge file state into global state
+
+						fclose($fd);
+					}
+				} // fopen()
+			} // foreach($state['sources'])
 		}
 
 		/// Return a string representing the state of the engine
@@ -63,7 +188,21 @@
 		function js()
 		{
 		}
-		///
+		/// Validate selector and prepare for use as a hash
+		function clean_selector($sel)
+		{
+			// TODO Do this properly
+			return $sel;
+		}
+	}
+
+	class PCP_Property
+	{
+		var $name;
+		var $value;
+		var $src;
+		var $ln;
+		var $cn;
 	}
 	/**
 	 * Get or set a single property or variable in a selector
